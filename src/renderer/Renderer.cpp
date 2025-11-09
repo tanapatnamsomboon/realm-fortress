7dd61a652c906ac349dd321c454ae3682c38f316
Tanapat Namsomboon <tanapat.nams@outlook.com>
2025-11-09 09:29:27 +0700

    feat(renderer): add Renderer class

---
+++ src/renderer/Renderer.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file Renderer.cpp
+ * @brief
+ * @date 11/9/2025
+ */
+
+#include "Renderer.h"
+#include <glad/gl.h>
+
+namespace RealmFortress
+{
+    void Renderer::Init()
+    {
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glFrontFace(GL_CCW);
+    }
+
+    void Renderer::Clear(const glm::vec3& color)
+    {
+        glClearColor(color.r, color.g, color.b, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+
+    void Renderer::Shutdown()
+    {
+    }
+} // namespace RealmFortress
